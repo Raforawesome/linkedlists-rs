74fdfdf792e29cb40560cef7c06f76d6f278b72e
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-21 11:21:30 -0400

    Test list creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 mod linked_list;
 mod alloc;
 
+pub use linked_list::{ LinkedList, LinkedNode };
+
 #[cfg(test)]
 mod tests {
+	use crate::{ LinkedList, LinkedNode };
+
     #[test]
     fn it_works() {
+		let list: LinkedList<i32> = LinkedList::new(LinkedNode::head(2));
 		todo!()
     }
 }
