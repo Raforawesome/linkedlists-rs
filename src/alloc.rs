92171ce651bce3788452c555a212485a05a9967c
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-21 10:51:20 -0400

    Create allocation module

---
+++ src/alloc.rs
@@ -0,0 +1,22 @@
+#![allow(unused, dead_code)]
+use std::alloc::{ self, Layout };
+
+pub fn raw_alloc<T>() -> *mut T {
+	unsafe {
+		alloc::alloc(size_oft::<T>()) as *mut T
+	}
+}
+
+pub fn raw_dealloc<T>(ptr: *mut T) {
+	unsafe {
+		alloc::dealloc(ptr as *mut u8, size_oft::<T>())
+	}
+}
+
+fn size_ofv<T>(_: T) -> Layout {
+	Layout::new::<T>()
+}
+
+fn size_oft<T>() -> Layout {
+	Layout::new::<T>()
+}
