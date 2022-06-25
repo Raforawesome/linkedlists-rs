9097e877fdc163660966b4d9073f1e86c20f5c7d
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-25 03:00:29 -0400

    Added Clone implementation, Drop implementation, and `LinkedList`

--- src/unsafe_linked_list.rs
+++ src/unsafe_linked_list.rs
@@ -2,16 +2,22 @@
 use crate::alloc;
 
 #[derive(Debug, Clone)]
-pub struct LinkedNode<T> {
+pub struct LinkedNode<T>
+where T: Clone
+{
 	data: T,
-	next: *mut LinkedNode<T>
+	next: *mut LinkedNode<T>,
+	has_next: bool,
 }
 
-impl<T> LinkedNode<T> {
+impl<T> LinkedNode<T>
+where T: Clone
+{
 	pub fn new(data: T) -> Self {
 		Self {
 			data,
-			next: alloc::null_ptr::<LinkedNode<T>>()
+			next: alloc::null_ptr::<LinkedNode<T>>(),
+			has_next: false,
 		}
 	}
 
@@ -27,5 +33,36 @@ impl<T> LinkedNode<T> {
 		let ptr: *mut LinkedNode<T> = alloc::raw_alloc();
 		unsafe { *ptr = node };
 		self.next = ptr;
+		self.has_next = true;
+	}
+}
+
+impl<T> Drop for LinkedNode<T>
+where T: Clone
+{
+    fn drop(&mut self) {
+		alloc::raw_dealloc(self.next);
+    }
+}
+
+
+struct LinkedList<T>
+where T: Clone
+{
+	head: LinkedNode<T>,
+	tail: *mut LinkedNode<T>,
+	size: usize,
+}
+
+impl<T> LinkedList<T>
+where T: Clone
+{
+	pub fn new(head: LinkedNode<T>) -> Self {
+		let ptr: *mut LinkedNode<T> = alloc::null_ptr();
+		Self {
+			head,
+			tail: ptr,
+			size: 1,
+		}
 	}
 }
