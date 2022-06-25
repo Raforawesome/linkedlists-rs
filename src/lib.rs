37cc0242ecbb4d81f5cfc14c6c61854e0fbd4923
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-25 01:43:59 -0400

    Update `lib.rs` to reflect file changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
-mod safe_linked_list;
 mod alloc;
-
-pub use safe_linked_list::{ LinkedList, LinkedNode };
+mod linked_list;
+mod unsafe_linked_list;
 
 #[cfg(test)]
 mod tests {
-	use crate::{ LinkedList, LinkedNode };
+	use super::*;
 
     #[test]
     fn it_works() {
-		let _list: LinkedList<i32> = LinkedList::new(LinkedNode::head(2));
     }
 
 	#[test]
 	fn index_test() {
-		let list: LinkedList<i32> = LinkedList::new(LinkedNode::head(2));
-		let index: Option<usize> = list.index(2);
-		dbg!(index);
 	}
 }
